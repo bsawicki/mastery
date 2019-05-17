a6f7d3e297ddceb14e3ac829d13be0a7dd623e80
Brian Sawicki <brian.sawicki@gmail.com>
2019-05-16 20:25:53 -0500

    Make truck matcher a class, add unit tests with test input/output

--- truck_matching.py
+++ truck_matching.py
@@ -2,49 +2,60 @@ import csv
 import sys
 from collections import defaultdict
 
-truck_dict = defaultdict(lambda: defaultdict(dict))
-distance_dict = defaultdict(defaultdict)
+class TruckMatcher:
+	# Parse the CSV file for trucks provided on the command line into truck_dict
+	def parse_trucks(self, truck_file):
+		self.truck_dict = defaultdict(lambda: defaultdict(dict))
+		with open(truck_file, newline='') as csvfile:
+			reader = csv.DictReader(csvfile)
+			for row in reader:
+				if row["Equipment"] in self.truck_dict[row["Origin"]][row["Destination"]]:
+					self.truck_dict[row["Origin"]][row["Destination"]][row["Equipment"]].append(row["Truck"])
+				else:
+					self.truck_dict[row["Origin"]][row["Destination"]][row["Equipment"]] = [row["Truck"]]
 
-# Parse the CSV file for trucks provided on the command line into truck_dict
-def parse_trucks(truck_csv):
-	with open(truck_csv, newline='') as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			if row["Equipment"] in truck_dict[row["Origin"]][row["Destination"]]:
-				truck_dict[row["Origin"]][row["Destination"]][row["Equipment"]].append(row["Truck"])
-			else:
-				truck_dict[row["Origin"]][row["Destination"]][row["Equipment"]] = [row["Truck"]]
+	# Parse the CSV file for distances provided on the command line into distance_dict
+	# We assume each origin-destination combination is unique
+	def parse_distances(self, distance_file):
+		self.distance_dict = defaultdict(defaultdict)
+		with open(distance_file, newline='') as csvfile:
+			reader = csv.DictReader(csvfile)
+			for row in reader:
+				self.distance_dict[row["Origin"]][row["Destination"]] = row["Distance"]
 
-# Parse the CSV file for distances provided on the command line into distance_dict
-# We assume each origin-destination combination is unique
-def parse_distances(distance_csv):
-	with open(distance_csv, newline='') as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			distance_dict[row["Origin"]][row["Destination"]] = row["Distance"]
+	# Match loads from the given CSV file with known trucks and print the match in the format "Load,[Truck(s)],Distance"
+	# Right now this outputs as a file and to the command line, we could return the values from the function if we wanted
+	def match_loads(self, load_file):
+		with open(load_file, newline='') as csvfile:
+			reader = csv.DictReader(csvfile)
+			# Open a file to output the results to and trucate it
+			output_file = load_file + ".output"
+			writer = open(output_file, "a")
+			writer.truncate(0)
+			for row in reader:
+				try:
+					trucks = self.truck_dict[row["Origin"]][row["Destination"]][row["Equipment"]]
+				except KeyError:
+					# This could be improved by saying whether a truck on the given route or the given equipment was not found
+					trucks = ["Not Found"]
+				try:
+					distance = self.distance_dict[row["Origin"]][row["Destination"]]
+				except KeyError:
+					distance = "Not Found"
+				result = row["Load"] + "," + str(trucks) + "," + distance
+				print(result)
+				writer.write(result + "\n")
+			writer.close()
 
-# Match loads from the given CSV file with known trucks and print the match in the format "Load,[Truck(s)],Distance"
-def match_loads(load_csv):
-	with open(load_csv, newline='') as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			try:
-				trucks = truck_dict[row["Origin"]][row["Destination"]][row["Equipment"]]
-			except KeyError:
-				# This could be improved by saying whether a truck on the given route or the given equipment was not found
-				trucks = ["Not Found"]
-			try:
-				distance = distance_dict[row["Origin"]][row["Destination"]]
-			except KeyError:
-				distance = "Not Found"
-			print(row["Load"] + "," + str(trucks) + "," + distance)
+	def __init__(self, truck_file, distance_file):
+		self.parse_trucks(truck_file)
+		self.parse_distances(distance_file)
 
-# Read command line args
-truck_file = sys.argv[1]
-distance_file = sys.argv[2]
-load_file = sys.argv[3]
+if __name__ == "__main__":
+	# Read command line arguments
+	truck_file = sys.argv[1]
+	distance_file = sys.argv[2]
+	load_file = sys.argv[3]
 
-# Parse CSVs and match loads
-parse_trucks(truck_file)
-parse_distances(distance_file)
-match_loads(load_file)
+	truck_matcher = TruckMatcher(truck_file, distance_file)
+	truck_matcher.match_loads(load_file)
